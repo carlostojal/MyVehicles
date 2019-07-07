84b5d95841dc63daf4f342e9e9b26efa245afb70
Carlos Tojal <carlos.tojal@hotmail.com>
2019-07-07 21:33:18 +0100

    Added other vehicles.

--- app/src/main/java/com/carlostojal/myvehicles/Other.java
+++ app/src/main/java/com/carlostojal/myvehicles/Other.java
@@ -8,6 +8,7 @@ package com.carlostojal.myvehicles;
 //
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -16,17 +17,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class Other extends Fragment {
 
-    ArrayList<Vehicle> others;
+    private ListView otherList;
+    private VehicleManager vehicleManager = new VehicleManager();
+    private ArrayList<Vehicle> others;
+    private Button addOther;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_other, container, false);
 
+        others = vehicleManager.loadVehicles(getContext(),3);
+        if(others.size()==0)
+            Toast.makeText(getContext(), "No vehicles were found.", Toast.LENGTH_SHORT).show();
+
+        otherList = view.findViewById(R.id.other_list);
+        ArrayAdapter otherAdapter = new OtherAdapter(getContext(),others);
+        otherList.setAdapter(otherAdapter);
+
+        addOther = (Button) view.findViewById(R.id.add_other);
+        addOther.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAddOther();
+            }
+        });
+
         return view;
+    }
+
+    public void onAddOther() {
+        Intent intent = new Intent(getContext(), AddVehicle.class);
+        intent.putExtra("type",3);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/carlostojal/myvehicles/OtherAdapter.java
+++ app/src/main/java/com/carlostojal/myvehicles/OtherAdapter.java
@@ -1,19 +1,28 @@
 package com.carlostojal.myvehicles;
 
+//
+// Copyright © Carlos Tojal (carlostojal)
+// OtherAdapter.java
+// MyVehicles
+// github.com/carlostojal/MyVehicles
+//
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class OtherAdapter {
+public class OtherAdapter extends ArrayAdapter<Vehicle> {
 
     private final Context context;
     private final ArrayList<Vehicle> others;
 
     public OtherAdapter(Context context, ArrayList<Vehicle> others) {
+        super(context,R.layout.vehicle,others);
         this.context = context;
         this.others = others;
     }
@@ -24,6 +33,10 @@ public class OtherAdapter {
 
         TextView brand_model = (TextView) rowView.findViewById(R.id.brand_model);
         TextView registration = (TextView) rowView.findViewById(R.id.registration);
+        TextView lastInsurance = (TextView) rowView.findViewById(R.id.lastInsurance);
+        TextView lastInspection = (TextView) rowView.findViewById(R.id.lastInspection);
+        TextView lastTax = (TextView) rowView.findViewById(R.id.lastTax);
+        TextView lastRevision = (TextView) rowView.findViewById(R.id.lastRevision);
 
         StringBuilder brand_model_text = new StringBuilder();
         brand_model_text.append(others.get(position).getBrand());
@@ -33,6 +46,66 @@ public class OtherAdapter {
 
         String registration_text = others.get(position).getRegistration();
         registration.setText(registration_text);
+
+        StringBuilder insurance_text = new StringBuilder();
+        insurance_text.append("Last insurance: ");
+        if (others.get(position).getInsurance().get(others.get(position).getInsurance().size() - 1).getDate().getDay() != 0) {
+            insurance_text.append(String.valueOf(others.get(position).getInsurance().get(others.get(position).getInsurance().size() - 1).getDate().getDay()));
+            insurance_text.append("/");
+            insurance_text.append(String.valueOf(others.get(position).getInsurance().get(others.get(position).getInsurance().size() - 1).getDate().getMonth()));
+            insurance_text.append("/");
+            insurance_text.append(String.valueOf(others.get(position).getInsurance().get(others.get(position).getInsurance().size() - 1).getDate().getYear()));
+            insurance_text.append(" - ");
+            insurance_text.append(String.valueOf(others.get(position).getInsurance().get(others.get(position).getInsurance().size() - 1).getValue()));
+        }
+        else
+            insurance_text.append("No information.");
+        lastInsurance.setText(insurance_text);
+
+        StringBuilder inspection_text = new StringBuilder();
+        inspection_text.append("Last inspection: ");
+        if(others.get(position).getInspection().get(others.get(position).getInspection().size()-1).getDate().getDay()!=0) {
+            inspection_text.append(String.valueOf(others.get(position).getInspection().get(others.get(position).getInspection().size() - 1).getDate().getDay()));
+            inspection_text.append("/");
+            inspection_text.append(String.valueOf(others.get(position).getInspection().get(others.get(position).getInspection().size() - 1).getDate().getMonth()));
+            inspection_text.append("/");
+            inspection_text.append(String.valueOf(others.get(position).getInspection().get(others.get(position).getInspection().size() - 1).getDate().getYear()));
+            inspection_text.append(" - ");
+            inspection_text.append(String.valueOf(others.get(position).getInspection().get(others.get(position).getInspection().size() - 1).getValue()));
+        }
+        else
+            inspection_text.append("No information.");
+        lastInspection.setText(inspection_text);
+
+        StringBuilder tax_text = new StringBuilder();
+        tax_text.append("Last tax: ");
+        if(others.get(position).getTax().get(others.get(position).getTax().size()-1).getDate().getDay()!=0) {
+            tax_text.append(String.valueOf(others.get(position).getTax().get(others.get(position).getTax().size() - 1).getDate().getDay()));
+            tax_text.append("/");
+            tax_text.append(String.valueOf(others.get(position).getTax().get(others.get(position).getTax().size() - 1).getDate().getMonth()));
+            tax_text.append("/");
+            tax_text.append(String.valueOf(others.get(position).getTax().get(others.get(position).getTax().size() - 1).getDate().getYear()));
+            tax_text.append(" - ");
+            tax_text.append(String.valueOf(others.get(position).getTax().get(others.get(position).getTax().size() - 1).getValue()));
+        }
+        else
+            tax_text.append("No information.");
+        lastTax.setText(tax_text);
+
+        StringBuilder revision_text = new StringBuilder();
+        revision_text.append("Last revision: ");
+        if(others.get(position).getRevision().get(others.get(position).getRevision().size()-1).getDate().getDay()!=0) {
+            revision_text.append(String.valueOf(others.get(position).getRevision().get(others.get(position).getRevision().size() - 1).getDate().getDay()));
+            revision_text.append("/");
+            revision_text.append(String.valueOf(others.get(position).getRevision().get(others.get(position).getRevision().size() - 1).getDate().getMonth()));
+            revision_text.append("/");
+            revision_text.append(String.valueOf(others.get(position).getRevision().get(others.get(position).getRevision().size() - 1).getDate().getYear()));
+            revision_text.append(" - ");
+            revision_text.append(String.valueOf(others.get(position).getRevision().get(others.get(position).getRevision().size() - 1).getValue()));
+        }
+        else
+            revision_text.append("No information.");
+        lastRevision.setText(revision_text);
 
         return rowView;
     }
